7e10a34aa676db35ade3944e012ad5fa14f19f69
resilienciaeth <dani@nakedmeta.io>
2022-08-31 20:58:28 -0400

    screen

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,9 @@ import Navbar from '../components/Navbar';
 import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps }) => (
-
-  <div className="bg-dime-2">
+  <div className="bg-dime-1">
     <Navbar />
-    <Component {...pageProps} />
+    <Component {...pageProps} className="bg-dime-2" />
 
   </div>
 );
--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,16 @@ import Image from 'next/image';
 import images from '../assets';
 
 const Home = () => (
-  <div className="flex justify-center flex-col">
-    <div className="w-full h-screen bg-dime-1 minmd:w-4/5">
-      <div className="justify-center  flex mt-[11rem]">
+  <div className="flex bg-dime-2 justify-center px-0 flex-col">
+    <div className="w-full h-[110vh] bg-dime-1 minmd:w-4/5">
+      <div className="justify-center  flex mt-[10rem]">
         <Image src={images.titulomobile} width={370} height={360} />
       </div>
       <div className="flex font-kanit text-white -mt-[1rem] font-semibold justify-center text-[13px]">
         <p className="">El primer Talk Show en el Metaverso</p>
 
       </div>
-      <div className="flex justify-center mt-[10rem]">
+      <div className="flex justify-center mt-[8rem]">
         <p className="font-kanit font-semibold text-[10px] text-white">Desliza y conoce más de este nuevo Show</p>
       </div>
     </div>
@@ -112,14 +112,14 @@ const Home = () => (
         </div>
       </div>
     </div>
-    <div className="h-[150vh] bg-dime-2">
+    <div className="h-screen mt-[8rem] bg-dime-2">
       <Image src={images.disponible} />
-      <div className="flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center mt-20 justify-center">
         <Image src={images.coffee} width={123} height={123} />
         <p className="text-center mt-5 px-[5rem] font-semibold text-dime-text font-kanit text-[15px]">Todos los jueves a las 9:00pm CDMX en tus plataformas de podcast favoritas</p>
       </div>
       <div className=" bg-dime-2 mt-[3rem] flex justify-start flex-row overflow-hidden no-scrollbar overflow-x-scroll">
-        <div className="text-black min-w-[289px] ml-10 px-4">
+        <div className="text-black bg-dime-2 min-w-[289px] ml-10 px-4">
           <Image src={images.pepeposter2} />
         </div>
         <div className="text-black min-w-[289px] px-4">
@@ -128,61 +128,61 @@ const Home = () => (
         <div className="text-black min-w-[289px] px-4">
           <Image src={images.pepeposter2} />
         </div>
-        <div className="text-black min-w-[289px] px-4">
+        <div className="text-black bg-dime-2 min-w-[289px] px-4">
           <Image src={images.pepeposter2} />
         </div>
       </div>
-      <div className="h-screen mt-[0rem] mb-0 bg-dime-2">
-        <div className="mt-10">
-          <Image src={images.acompananos} width={500} height={100} />
-        </div>
-        <div className="flex justify-start items-start px-10">
-          <div className="bg-dime-1 mt-10 flex justify-start flex-col rounded-3xl w-full px-4 h-[30rem]">
-            <p className="mt-10 text-center font-kanit text-[12px] font-semibold text-dime-2">
-              Quieres asistir al programa en vivo?
-            </p>
-            <div className="mt-10 px-2 flex flex-col items-start">
-              <div className="flex flex-col">
-                <p className="text-[10px] py-3 font-kanit font-semibold text-dime-2">Nombre y Apellidos</p>
-                <input type="text" id="nombre" required />
-              </div>
-              <div className="flex flex-col">
-                <p className="text-[10px] py-3 font-kanit font-semibold text-dime-2">Edad</p>
-                <input type="text" id="nombre" required />
-              </div>
-              <div className="flex flex-col">
-                <p className="text-[10px] py-3 font-kanit font-semibold text-dime-2">Correo electronico</p>
-                <input type="text" id="nombre" required />
-              </div>
-              <div className="flex flex-col">
-                <p className="text-[10px] py-3 font-kanit font-semibold text-dime-2">Cuentas con un Oculus Quest 2?</p>
-                <input type="text" id="nombre" required />
-              </div>
+    </div>
+    <div>
+      <div className="flex bg-dime-2 mt-[12rem]">
+        <Image src={images.acompananos} width={500} height={100} />
+      </div>
+      <div className="flex justify-start items-start px-10">
+        <div className="bg-dime-1 mt-10 flex justify-start flex-col rounded-3xl w-full px-4 h-[30rem]">
+          <p className="mt-10 text-center font-kanit text-[12px] font-semibold text-dime-2">
+            Quieres asistir al programa en vivo?
+          </p>
+          <div className="mt-10 px-2 flex flex-col items-start">
+            <div className="flex flex-col">
+              <p className="text-[10px] py-3 font-kanit font-semibold text-dime-2">Nombre y Apellidos</p>
+              <input type="text" id="nombre" required />
             </div>
-            <div className="flex justify-end items-end mt-10">
-              <button type="submit" className="bg-button-1 px-10 py-2 rounded-3xl font-kanit font-bold text-lg">Enviar</button>
+            <div className="flex flex-col">
+              <p className="text-[10px] py-3 font-kanit font-semibold text-dime-2">Edad</p>
+              <input type="text" id="nombre" required />
             </div>
+            <div className="flex flex-col">
+              <p className="text-[10px] py-3 font-kanit font-semibold text-dime-2">Correo electronico</p>
+              <input type="text" id="nombre" required />
+            </div>
+            <div className="flex flex-col">
+              <p className="text-[10px] py-3 font-kanit font-semibold text-dime-2">Cuentas con un Oculus Quest 2?</p>
+              <input type="text" id="nombre" required />
+            </div>
+          </div>
+          <div className="flex justify-end items-end mt-10">
+            <button type="submit" className="bg-button-1 px-10 py-2 rounded-3xl font-kanit font-bold text-lg">Enviar</button>
           </div>
         </div>
       </div>
-      <div className="h-[400px] mt-10">
-        <div className="flex flex-row justify-between items-center px-12">
-          <p className="font-kanit text-[12px]">Inicio</p>
-          <p className="font-kanit text-[12px]">Youtube</p>
-          <p className="font-kanit text-[12px]">Spotify</p>
-          <p className="font-kanit text-[12px]">Acompañanos</p>
-        </div>
-        <div className="flex items-center justify-center mt-12">
-          <Image src={images.logofooter} width={300} height={80} />
-        </div>
-        <div className="flex items-center flex-row justify-between px-8 mt-12">
-          <Image src={images.spatial} width={90} height={34} onClick />
-          <Image src={images.nakedmeta} width={120} height={20} />
-          <Image src={images.ready} width={50} height={34} />
-        </div>
-        <div className="flex justify-center mt-12">
-          <p className="font-kanit text-[12px]">2022. Dime La Meta. Todos los derechos reservados.</p>
-        </div>
+    </div>
+    <div className="h-[400px] bg-white mt-20">
+      <div className="flex mt-10 flex-row justify-between items-center px-12">
+        <p className="font-kanit text-[12px]">Inicio</p>
+        <p className="font-kanit text-[12px]">Youtube</p>
+        <p className="font-kanit text-[12px]">Spotify</p>
+        <p className="font-kanit text-[12px]">Acompañanos</p>
+      </div>
+      <div className="flex items-center justify-center mt-12">
+        <Image src={images.logofooter} width={300} height={80} />
+      </div>
+      <div className="flex items-center flex-row justify-between px-8 mt-12">
+        <Image src={images.spatial} width={90} height={34} onClick />
+        <Image src={images.nakedmeta} width={120} height={20} />
+        <Image src={images.ready} width={50} height={34} />
+      </div>
+      <div className="flex justify-center mt-12">
+        <p className="font-kanit text-[12px]">2022. Dime La Meta. Todos los derechos reservados.</p>
       </div>
     </div>
   </div>
